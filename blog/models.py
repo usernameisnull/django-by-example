8e97deecb3100a82044366678002e5721360641d
mabing <cumt_ttr@163.com>
2016-04-19 10:47:33 -0700

    Canonical URLs for models

--- blog/models.py
+++ blog/models.py
@@ -34,3 +34,10 @@ class Post(models.Model):
         ordering = ('-publish',)
     def __str__(self):
         return self.title
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                        args=[self.publish.year,
+                              self.publish.strftime('%m'),
+                              self.publish.strftime('%d'),
+                              self.slug])
+
