75b717b8cf099f593c4f1f8697e01efb9d6e2787
mabing <cumt_ttr@163.com>
2016-04-19 11:23:57 -0700

    添加文件编码，添加检测是否给py文件设置编码的工具tools.sh

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+# encoding:utf-8
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 # Create your views here.
---
+++ tools.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#用来检查工程里的python文件的第一行有没有指定编码为 utf-8,因为代码里有汉字注释。
+x=$(find . -name '*.py')
+add_utf8(){
+    sed -i '1 i #encoding: utf-8' $1
+}
+for item in ${x[*]}
+do
+    head -n 1 "$item"| grep utf-8
+    if [[ "$?" -ne 0 ]]; then
+        # 添加
+        lines=$(wc -l "$item")
+        # 如果py文件为空
+        if [[ "$lines" -eq 0 ]]; then
+            echo "#encoding: utf-8" >> "$item"
+        else
+            add_utf8 "$item"
+        fi
+    fi
+done
+
